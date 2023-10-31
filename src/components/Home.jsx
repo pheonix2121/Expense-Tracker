31d2fca495d83cf8d1a61428596d2f8b93512e1a
pheonix2121 <bobbypanwar73@gmail.com>
2023-10-31 21:22:46 +0530

    e.t task 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Home=()=>{
-return (<div>Welcome</div>)
+import AuthContext from "../store/AuthContext";
 
-}
+const Home = () => {
+  const navigate = useNavigate();
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    navigate("/");
+  };
+
+  return (
+    <div >
+        <h1>welcome to expense tracker </h1>
+
+      <nav >
+        <ul>
+          {!isLoggedIn && (
+            <Link to="/" >
+              Login
+            </Link>
+          )}
+          {isLoggedIn && (
+            <button onClick={logoutHandler}>Logout</button>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Home;
